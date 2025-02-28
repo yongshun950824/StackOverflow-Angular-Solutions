25f5aceafbe5c81cae008b43843f8a388a81eb04
yongshun950824 <yongshun950824@gmail.com>
2025-02-28 21:53:56 +0800

    Update package 76849187

--- 76849187/src/main.ts
+++ 76849187/src/main.ts
@@ -12,10 +12,7 @@ import { HttpClientModule, provideHttpClient } from '@angular/common/http';
   standalone: true,
   imports: [CommonModule, HttpClientModule],
   template: `
-    <h1>Hello from {{name}}!</h1>
-    <a target="_blank" href="https://angular.io/start">
-      Learn more about Angular 
-    </a>
+    {{ dataSource | json }}
   `,
 })
 export class App {
