5f3d2414621da63dada56ed9034f9bd41c78b691
yongshun950824 <yongshun950824@gmail.com>
2025-04-19 15:09:36 +0800

    Update package and Resolve test 69275784

--- 69275784/src/main.ts
+++ 69275784/src/main.ts
@@ -1,20 +1,21 @@
-// import './polyfills';
+import './polyfills';
 
-// import { enableProdMode } from '@angular/core';
-// import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-// import { AppModule } from './app/app.module';
+import { AppModule } from './app/app.module';
 
-// platformBrowserDynamic().bootstrapModule(AppModule).then(ref => {
-//   // Ensure Angular destroys itself on hot reloads.
-//   if (window['ngRef']) {
-//     window['ngRef'].destroy();
-//   }
-//   window['ngRef'] = ref;
+platformBrowserDynamic().bootstrapModule(AppModule).then(ref => {
+  // Ensure Angular destroys itself on hot reloads.
+  if (window['ngRef']) {
+    window['ngRef'].destroy();
+  }
+  window['ngRef'] = ref;
 
-//   // Otherwise, log the boot error
-// }).catch(err => console.error(err));
+  // Otherwise, log the boot error
+}).catch(err => console.error(err));
 
+/*
 import './haxx/global-jasmine';
 import 'jasmine-core/lib/jasmine-core/jasmine-html.js';
 import 'jasmine-core/lib/jasmine-core/boot.js';
@@ -60,3 +61,4 @@ function bootstrap() {
     platformBrowserDynamicTesting()
   );
 }
+*/
