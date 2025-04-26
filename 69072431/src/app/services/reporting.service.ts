8d34bf17096d61bd2ec26454befff2dc1a3a14aa
yongshun950824 <yongshun950824@gmail.com>
2025-04-26 17:29:35 +0800

    Update package 69072431

--- 69072431/src/app/services/reporting.service.ts
+++ 69072431/src/app/services/reporting.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { map, Observable } from 'rxjs';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { visionCompacteDTOs } from '../models/model';
 
 @Injectable({
