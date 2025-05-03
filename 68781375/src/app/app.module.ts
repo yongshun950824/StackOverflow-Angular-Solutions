e54521ba0beeb6c5d7d8d04185c1dc2117ea0f88
yongshun950824 <yongshun950824@gmail.com>
2025-05-03 10:12:27 +0800

    Update package 68781375

--- 68781375/src/app/app.module.ts
+++ 68781375/src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TableModule } from 'primeng/table';
 import { DynamicGridComponent } from './dynamic-grid/dynamic-grid.component';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, BrowserAnimationsModule, TableModule],
+  imports: [BrowserModule, FormsModule, BrowserAnimationsModule, TableModule, CommonModule],
   declarations: [AppComponent, DynamicGridComponent],
   bootstrap: [AppComponent]
 })
