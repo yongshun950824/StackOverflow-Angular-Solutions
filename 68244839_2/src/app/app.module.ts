3790ff5052be3e5471faa660f0b20b0c466022a6
yongshun950824 <yongshun950824@gmail.com>
2025-05-10 16:09:46 +0800

    Update packages 68244839

--- 68244839_2/src/app/app.module.ts
+++ 68244839_2/src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { ChartsModule } from 'ng2-charts';
+import { NgChartsModule } from 'ng2-charts';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, ChartsModule ],
+  imports:      [ BrowserModule, FormsModule, NgChartsModule ],
   declarations: [ AppComponent, HelloComponent ],
   bootstrap:    [ AppComponent ]
 })
