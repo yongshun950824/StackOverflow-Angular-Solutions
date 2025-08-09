7b48326951bf2a5658696386eaa5cdb2b881b3bf
yongshun950824 <yongshun950824@gmail.com>
2025-08-09 10:23:11 +0800

    Update package 79726695

--- 79726695/src/main.ts
+++ 79726695/src/main.ts
@@ -1,32 +1,7 @@
 import './polyfills';
 
-import {HttpClientModule} from '@angular/common/http';
-import {NgModule} from '@angular/core';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {MatNativeDateModule} from '@angular/material/core';
-import {BrowserModule} from '@angular/platform-browser';
-import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {DemoMaterialModule} from './material-module';
-
-import {TableExpandableRowsExample} from './app/table-expandable-rows-example';
-
-@NgModule({
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    FormsModule,
-    HttpClientModule,
-    DemoMaterialModule,
-    MatNativeDateModule,
-    ReactiveFormsModule,
-  ],
-  entryComponents: [TableExpandableRowsExample],
-  declarations: [TableExpandableRowsExample],
-  bootstrap: [TableExpandableRowsExample],
-  providers: []
-})
-export class AppModule {}
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app/app.module';
 
 platformBrowserDynamic().bootstrapModule(AppModule);
 
